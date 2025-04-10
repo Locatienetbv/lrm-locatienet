cbfd537e47e93ca726f051a58cd8325121a20eef
Remco Zut <remcozut@users.noreply.github.com>
2025-04-10 09:15:50 +0200

    typo

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -17,8 +17,8 @@ git rebase origin/gh-pages
 
 mkdir -p dist
 mkdir -p _data
-cp -a $CWD/dist/lrm-graphhopper.js dist/lrm-graphhopper-$VERSION.js
-cp -a $CWD/dist/lrm-graphhopper.min.js dist/lrm-graphhopper-$VERSION.min.js
+cp -a $CWD/dist/lrm-locatienet.js dist/lrm-locatienet-$VERSION.js
+cp -a $CWD/dist/lrm-locatienet.min.js dist/lrm-locatienet-$VERSION.min.js
 echo -e "- version: $VERSION\n" >>_data/versions.yml
 
 echo `pwd`
